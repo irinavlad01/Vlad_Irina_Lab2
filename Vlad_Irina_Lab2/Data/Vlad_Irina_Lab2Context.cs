11e62e4da144ea4e25a0a2f638795ff10690ed7b
Irina Vlad <irina.vlad99@gmail.com>
2022-12-23 20:21:57 +0200

    Schimbari pana la punctul 17

---
+++ Vlad_Irina_Lab2/Data/Vlad_Irina_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vlad_Irina_Lab2.Models;
+
+namespace Vlad_Irina_Lab2.Data
+{
+    public class Vlad_Irina_Lab2Context : DbContext
+    {
+        public Vlad_Irina_Lab2Context (DbContextOptions<Vlad_Irina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vlad_Irina_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Vlad_Irina_Lab2/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vlad_Irina_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Price { get; set; }
+    }
+}
