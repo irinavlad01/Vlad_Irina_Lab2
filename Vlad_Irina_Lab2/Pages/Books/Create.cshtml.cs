3667646ea0fbbf511bc678155139f5e4cf84ebfa
Irina Vlad <irina.vlad99@gmail.com>
2023-01-04 20:31:15 +0200

    Laborator 2 complet

--- Vlad_Irina_Lab2/Pages/Books/Create.cshtml.cs
+++ Vlad_Irina_Lab2/Pages/Books/Create.cshtml.cs
@@ -22,6 +22,7 @@ namespace Vlad_Irina_Lab2.Pages.Books
         public IActionResult OnGet()
         {
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "FirstName");
             return Page();
         }
 
