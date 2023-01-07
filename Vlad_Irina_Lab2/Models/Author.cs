d46cdcd812efd200ace05db33ecd02d4774cb646
Irina Vlad <irina.vlad99@gmail.com>
2023-01-07 02:18:07 +0200

    Operatiunea de select pe baza editurii done

--- Vlad_Irina_Lab2/Models/Author.cs
+++ Vlad_Irina_Lab2/Models/Author.cs
@@ -12,6 +12,14 @@ namespace Vlad_Irina_Lab2.Models
         public string? FirstName { get; set; }
         [Display(Name = "Last Name")]
         public string? LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName ; 
+            }
+        }
 
 
         public ICollection<Book>? Books { get; set; }
